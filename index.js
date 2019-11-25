cd7921d29b3bb1d9da95874a0964cd915d950bd7
gortron <gordon.lanza@gmail.com>
2019-11-25 17:38:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const findMatching = (source, sought) => {
+  return source.filter(possibleMatch =>
+    possibleMatch.toLowerCase() === sought.toLowerCase())
+}
+
+const fuzzyMatch = (source, sought) => {
+  return source.filter(possibleMatch =>
+    possibleMatch[0].toLowerCase() === sought[0].toLowerCase())
+}
+
+const matchName = (source, sought) => {
+  return source.filter(possibleMatch =>
+    possibleMatch === sought)
+}
+
+// findMatching()
+// 1) returns all drivers that match the passed in name
+// 2) returns matching drivers if case does not match but letters do
+// 3) returns an empty array if there is no match
+// fuzzyMatch()
+// 4) returns a driver if beginning provided letters match
+// 5) does not return drivers if only middle or ending letters match
+// 6) does not return drivers if only middle or ending letters match
+// matchName()
+// 7) accesses the data structure to check if name matches
